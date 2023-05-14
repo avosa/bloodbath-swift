c0c38bf18828e5fe1a51461aa769cf6e7d78a922
Webster <websterb17@gmail.com>
2023-05-14 03:09:41 -0400

    Add Tests

--- Tests/BloodbathSwiftTests/BloodbathSwiftTests.swift
+++ Tests/BloodbathSwiftTests/BloodbathSwiftTests.swift
@@ -2,11 +2,73 @@ import XCTest
 @testable import BloodbathSwift
 
 final class BloodbathSwiftTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documenation
-        // https://developer.apple.com/documentation/xctest
+    var bloodbath: Bloodbath!
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    override func setUp() {
+        super.setUp()
+        bloodbath = BloodbathSwift.shared
+        bloodbath.config.apiKey = "test-api-key"
+    }
+
+    override func tearDown() {
+        bloodbath = nil
+        super.tearDown()
+    }
+
+    func testScheduleEvent() {
+        let expectation = self.expectation(description: "Schedule Event")
+        let event = Event(args: ["key": "value"])
+        bloodbath.event().schedule(event: event) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure(let error):
+                XCTFail("Error: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testFindEvent() {
+        let expectation = self.expectation(description: "Find Event")
+        bloodbath.event().find(id: "test-event-id") { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure(let error):
+                XCTFail("Error: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testListEvents() {
+        let expectation = self.expectation(description: "List Events")
+        bloodbath.event().list() { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure(let error):
+                XCTFail("Error: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testCancelEvent() {
+        let expectation = self.expectation(description: "Cancel Event")
+        bloodbath.event().cancel(id: "test-event-id") { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure(let error):
+                XCTFail("Error: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
     }
 }
